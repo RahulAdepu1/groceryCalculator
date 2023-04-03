dd4b1a7343df265c2b97fe53a6a4e8176a9e5e19
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-04-03 12:23:00 -0500

    added main screen View

--- groceryCalculator/groceryCalculator/PantryView.swift
+++ groceryCalculator/groceryCalculator/PantryView.swift
@@ -12,7 +12,31 @@ struct PantryView: View {
     @EnvironmentObject var listNameCoreDataVM: ListNameCoreDataVM
     
     var body: some View {
-        Text("Hello")
+        ZStack {
+            Color.clear
+            let pantry = listNameCoreDataVM.pantryCoreData
+            if pantry.isEmpty {
+                ZStack {
+                    Color.gray.ignoresSafeArea()
+                    Text("Empty")
+                }
+            }else {
+                VStack {
+                    ScrollView{
+                        ForEach(listNameCoreDataVM.pantryCoreData) { item in
+                            Text("Item 1")
+                                .frame(maxWidth: .infinity)
+                                .background(Color.black.opacity(0.6))
+                        }
+                        
+                    }.padding(.top, 30)
+                }
+                .frame(maxWidth: .infinity)
+                .background(Color.black.opacity(0.4))
+            }
+        }
+        .navigationTitle("Pantry")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
